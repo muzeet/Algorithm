c335a91d1f9222390ab2420b3780f10a8e74db73
taoxiangdong <taoxiangdong1@huawei.com>
2020-07-09 21:44:51 +0800

    new commit

--- src/剑指Offer/从尾到头打印链表/PrintListFromTailToHead.java
+++ src/剑指Offer/从尾到头打印链表/PrintListFromTailToHead.java
@@ -1,15 +1,56 @@
 package 剑指Offer.从尾到头打印链表;
 
-class ListNode {
-        int val;
-        ListNode next = null;
+import javax.lang.model.type.ArrayType;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Stack;
 
-        ListNode(int val) {
-            this.val = val;
-        }
+class ListNode {
+    int val;
+    ListNode next = null;
+
+    ListNode(int val) {
+        this.val = val;
     }
+}
 
 public class PrintListFromTailToHead {
 
+    public void addLastNode(ListNode listNode, ArrayList<Integer> list) {
+        if (listNode.next != null) {
+            addLastNode(listNode.next, list);
+        }
+        list.add(listNode.val);
+    }
 
+    public ArrayList<Integer> printListFromTailToHead(ListNode listNode) {
+        // 使用递归
+        ArrayList<Integer> list = new ArrayList<>();
+        if (listNode == null) {
+            return list;
+        }
+
+        addLastNode(listNode, list);
+        return list;
+    }
+
+
+    public ArrayList<Integer> printListFromTailToHead2(ListNode listNode) {
+        // 采用stack
+        ArrayList<Integer> res = new ArrayList<>();
+        if (listNode == null) {
+            return res;
+        }
+        Stack<ListNode> stack = new Stack<>();
+        while (listNode.next != null) {
+            stack.push(listNode);
+            listNode = listNode.next;
+        }
+
+
+        while (!stack.empty()) {
+            ListNode pNodes = stack.pop();
+            res.add(pNodes.val);
+        }
+    }
 }
