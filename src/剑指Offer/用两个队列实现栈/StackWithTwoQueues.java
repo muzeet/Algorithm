2c406f1e456c60f631881483aeb44d346bbbde61
taoxiangdong <taoxiangdong1@huawei.com>
2020-08-28 20:48:40 +0800

    update

--- src/剑指Offer/用两个队列实现栈/StackWithTwoQueues.java
+++ src/剑指Offer/用两个队列实现栈/StackWithTwoQueues.java
@@ -1,31 +1,39 @@
 package 剑指Offer.用两个队列实现栈;
 
 import java.util.LinkedList;
-import java.util.Stack;
 
 public class StackWithTwoQueues {
 
+    // 数据的插入原则：保持一个队列为空，一个队列不为空，往不为空的队列中插入元素
     LinkedList<Integer> queue1 = new LinkedList<>();
     LinkedList<Integer> queue2 = new LinkedList<>();
 
     public void push(int node) {
-        stack1.push(node);
+        if (!queue1.isEmpty()) {
+            queue1.addLast(node);
+        } else {
+            queue2.addLast(node);
+        }
     }
 
     public int pop() {
-        if (!stack2.isEmpty()) {
-            return stack2.pop();
-        }
-        while (!stack1.empty()) {
-            int pop = stack1.pop();
-            stack2.push(pop);
-        }
+        if (!queue1.isEmpty()) {
+            while (queue1.size() > 1) {
+                int first = queue1.pop();
+                queue2.addLast(first);
+            }
+            return queue1.pop();
 
-        if (stack2.isEmpty()) {
+        } else if (!queue2.isEmpty()) {
+            while (queue2.size() > 1) {
+                int first = queue2.pop();
+                queue1.addLast(first);
+            }
+            return queue2.pop();
+        } else {
+            // queue1 and queue2 both empty
             return -1;
         }
-
-        return stack2.pop();
     }
 
 }
