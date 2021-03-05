fc44b90b4330d4edd24167dfb89df291d2aeeaba
taoxiangdong <taoxiangdong1@huawei.com>
2021-03-05 10:33:39 +0800

    update code

---
+++ src/边角/IntegerTest.java
@@ -0,0 +1,14 @@
+package 边角;
+
+public class IntegerTest {
+    public static void main(String[] args) {
+        Integer value1 = 100;
+        Integer value2 = 100;
+        Integer value3 = 150;
+        Integer value4 = 150;
+        System.out.println(value1 == value2);
+        System.out.println(value3 == value4);
+        System.out.println(value3 == 150);
+    }
+
+}
